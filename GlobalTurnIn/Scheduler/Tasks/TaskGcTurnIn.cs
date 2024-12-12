87ceaaf9a489bca8c58edf856f10d9346c81f83b
UcanPatatess <ucanpatatess@gmail.com>
2024-12-12 20:06:45 +0300

    delay

--- GlobalTurnIn/Scheduler/Tasks/TaskGcTurnIn.cs
+++ GlobalTurnIn/Scheduler/Tasks/TaskGcTurnIn.cs
@@ -17,7 +17,7 @@ namespace GlobalTurnIn.Scheduler.Tasks
         }
         private static TaskManagerConfiguration LSConfig => new(timeLimitMS: 2 * 60 * 1000);
         private static TaskManagerConfiguration DConfig => new(timeLimitMS: 10 * 60 * 1000, abortOnTimeout: false);
-        private static void GoToGC() => P.taskManager.InsertMulti([new(() => P.lifestream.ExecuteCommand("gc")), new(() => P.lifestream.IsBusy()), new(() => !P.lifestream.IsBusy(), LSConfig)]);
+        private static void GoToGC() => P.taskManager.InsertMulti([new(PlayerNotBusy),new(() => P.lifestream.ExecuteCommand("gc")), new(() => P.lifestream.IsBusy()), new(() => !P.lifestream.IsBusy(), LSConfig)]);
         private static void Deliveroo() => P.taskManager.InsertMulti([new(() => Svc.Commands.ProcessCommand("/deliveroo enable")), new(() => P.deliveroo.IsTurnInRunning()), new(() => !P.deliveroo.IsTurnInRunning(), DConfig)]);
     }
 }
