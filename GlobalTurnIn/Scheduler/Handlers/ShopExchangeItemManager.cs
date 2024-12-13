6f982ab8a309676b779ed01d07cf9e3a0869665c
UcanPatatess <ucanpatatess@gmail.com>
2024-12-13 10:28:54 +0300

    some changes

--- GlobalTurnIn/Scheduler/Handlers/ShopExchangeItemManager.cs
+++ GlobalTurnIn/Scheduler/Handlers/ShopExchangeItemManager.cs
@@ -4,18 +4,34 @@ using ECommons.Logging;
 
 namespace GlobalTurnIn.Scheduler.Handlers
 {
-    internal static unsafe class SelectYesnoManager
+    internal static unsafe class ShopExchangeItemManager
     {
         internal static long NoSelectYesno = long.MaxValue;
+        internal static long NoShopExchangeItemDialog = long.MaxValue;
         internal static void Tick()
         {
             if (SchedulerMain.EnableTicking)
             {
                 if (IsAddonActive("ShopExchangeItem"))
                 {
+                    if (TryGetAddonByName<AtkUnitBase>("ShopExchangeItemDialog",out var addon)&&IsAddonReady(addon))
+                    {
+                        if (Environment.TickCount64 - NoShopExchangeItemDialog > 10)
+                        {
+                            if (GenericThrottle)
+                            {
+                                Callback.Fire(addon, true, 0);
+                                NoShopExchangeItemDialog = Environment.TickCount64;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        NoShopExchangeItemDialog = Environment.TickCount64;
+                    }
                     if (TryGetAddonByName<AtkUnitBase>("SelectYesno", out var addon2) && IsAddonReady(addon2))
                     {
-                        if (Environment.TickCount64 - NoSelectYesno > 100)
+                        if (Environment.TickCount64 - NoSelectYesno > 10)
                         {
                             if (GenericThrottle)
                             {
