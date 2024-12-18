61c99aa5093109d904d3560d9b3f18d8d1e8840d
Leontopodium Nivale <103514618+LeontopodiumNivale14@users.noreply.github.com>
2024-12-17 21:50:57 -0500

    some debug text

---
+++ GlobalTurnIn/Scheduler/Tasks/TaskA4N.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTurnIn.Scheduler.Tasks
+{
+    internal static class TaskA4N
+    {
+        internal static void Enqueue()
+        {
+            //P.taskManager.Enqueue(() => )
+        }
+    }
+}
